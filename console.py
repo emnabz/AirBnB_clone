047072fecafd71570500f210dfef4f4d9658bbe7
emnabz <1947@holbertonschool.com>
2021-02-11 17:42:46 +0100

    task6 console

---
+++ console.py
@@ -0,0 +1,20 @@
+import cmd
+from baseModel1 import baseModel1
+
+
+class HBNBcommand(cmd.Cmd):
+    
+    intro = 'Welcome to the hbnb shell. Type help or ? to list commands.
+    prompt = '(hbnb)'
+    
+# ----- basic turtle commands -----
+def do_quit(self, arg):
+    'Quit command to exit the program\n'
+    return True
+
+def do_EOF(self,arg):
+    'Quit command to exit the program\n'
+    return True
+
+def emptyline(self):
+    pass
