79a555335899c60c696d548b6b73ccd36976eaa1
emnabz <1947@holbertonschool.com>
2021-02-13 22:42:40 +0100

    task8

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+class user(BaseModel):
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+def __init__(self, *args, **kwargs):
+    super().__init__(self, *args, **kwargs)
